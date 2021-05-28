fde686cf72f2eb564a6e960f81f837decde4b5e9
Joseph-Developer <jose.garcia@pih.org>
2021-05-28 14:39:28 -0500

    Medicamento cantidad 0 --correcto

--- configuration/reports/reportdescriptors/dataexports/sql/mexicoCesMeds.sql
+++ configuration/reports/reportdescriptors/dataexports/sql/mexicoCesMeds.sql
@@ -5,8 +5,8 @@ SELECT d.name,
 	LEFT JOIN obs drug_name on d.drug_id = drug_name.value_drug
 		and date(drug_name.obs_datetime) BETWEEN @startDate AND @endDate  
    	LEFT JOIN location l
-                   ON l.location_id = obs.location_id
+                   ON l.location_id = drug_name.location_id
 	LEFT JOIN obs drug_quantity ON drug_name.obs_group_id=drug_quantity.obs_group_id
 WHERE d.name LIKE 'CES%' AND d.retired=0
-GROUP BY d.drug_id
+GROUP BY d.drug_id, l.location_id
 ORDER by d.name asc;
